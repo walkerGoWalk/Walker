823c96c7169fee0969c4e975145925202befca0f
walkerGoWalk <y.kravchenko.mail@gmail.com>
2020-01-19 22:50:58 +0100

    second commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain2.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain2.java
@@ -1,6 +1,6 @@
 package com.kodilla.testing;
 
-public class TestingMain {
+public class TestingMain2 {
     public static void main(String[] args){
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
     }
